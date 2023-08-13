96320c47fcc04a7d5bd999e778e4d40d22831c1e
Jgilbert18 <106094806+Jgilbert18@users.noreply.github.com>
2023-08-13 01:18:48 -0400

    Update Adder_Subtractor.v

--- Adder_Subtractor.v
+++ Adder_Subtractor.v
@@ -1,11 +1,11 @@
 module Adder_Subtractor (a, b, s, cout, cb);
-	input [7:0] a ,b;
-	input cb;
+	input [7:0] a ,b;  // 8 bits for the operand a and b
+	input cb;	   // The control bit
 	
-	output [7:0] s;
-	output cout;
+	output [7:0] s;   // 8 bits for the result of the a & b operation
+	output cout;      // Carry-out bit
 	
-	assign s = a ^ b ^ cb;
-	assign co = (a&b) | (a&cb) | (b&cb);
+	assign s = a ^ b ^ cb;      // Calculates bitwise XOR operation bewteen a, b, and control bit without considering carry
+	assign co = (a&b) | (a&cb) | (b&cb);  // calculates the carry using bitwise AND and OR operations between operand a, operand b, and the control bit 
 	
 endmodule
